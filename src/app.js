fc4c8ccba8cdf32271f70d76e987b51c303c9a8c
Mark Isaac <markisaac695@gmail.com>
2025-09-16 17:51:57 +0300

    feat(security): add helmet and express rate limiter

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import AppError from "./utils/appError.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimiter from "express-rate-limit";
+
 const app = express();
+
+app.use(helmet());
 
 app.use(express.json({
     limit: "10kb"
@@ -16,6 +21,14 @@ app.use(cookieParser());
 if(process.env.NODE_ENV === "development"){
     app.use(morgan("dev"));
 }
+
+// for limiting no of req
+const limiter = rateLimiter({
+    max: 5,
+    windowMs: 60 * 60 * 1000,
+    message: 'too many requests with this ip , try again in an hour'
+  });
+  app.use('/api', limiter);
 
 app.use("/api/auth", authRoutes);
 
